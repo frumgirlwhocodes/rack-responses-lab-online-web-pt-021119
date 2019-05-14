30dd8c7e0411cdf763f08666ad7d359dc8892835
lieby weintraub <liebahinda@gmail.com>
2019-05-14 18:51:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,8 @@ class Application
   
   def call(env)
      resp = Rack::Response.new
-     if Time <=12
+     time=Time.now
+     if time.hour <=12
      resp.write "Good Morning!" 
    else 
      resp.write "Good Afternoon!" 
